e55ce4a9e3c906bde612dd16ad39e38493f3095a
Siddharth-M.S <siddharthkani2004@gmail.com>
2024-02-15 22:53:35 +0530

    updated userModel-register

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const errorHandler = require('./middleware/errorhandler')
 const app = express()
 const port = process.env.PORT || 3001
 app.use(express.json())
+
 app.use('/api/contacts', require('./routes/contactRoutes'))
+app.use('/api/user', require('./routes/userRoutes'))
 app.use(errorHandler)
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
